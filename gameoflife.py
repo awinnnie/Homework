fdf9fec5b1743316b99103ae7709d27dd46f6418
awinnnie <114687525+awinnnie@users.noreply.github.com>
2022-11-17 18:33:45 +0400

    Update gameoflife.py

--- gameoflife.py
+++ gameoflife.py
@@ -1,6 +1,7 @@
 from mimetypes import init
 import pygame
 import sys
+import time
 
 pygame.init()
 
@@ -8,10 +9,11 @@ screen = pygame.display.set_mode((720, 500))
 r = pygame.Rect(0, 0, 500, 500)
 
 class Square():
-    def __init__(self, X_coord, Y_coord, white=False):
+    def __init__(self, X_coord, Y_coord, white=False, number_of_neigh=0):
         self.X_coord = X_coord
         self.Y_coord = Y_coord
         self.white = white
+        self.number = number_of_neigh
 
     def is_neigh(self,other):
         if (other.X_coord == self.X_coord) and (abs(other.Y_coord - self.Y_coord)==50):
@@ -21,7 +23,7 @@ class Square():
         elif abs(other.X_coord - self.X_coord)==50 and abs(other.Y_coord - self.Y_coord)==50:
             return True
         return False
-        
+
     def show(self):
         return (self.X_coord, self.Y_coord, self.white)
    
@@ -39,6 +41,14 @@ while True:
             pos = pygame.mouse.get_pos()
             pygame.draw.rect(screen, (200, 200, 200), pygame.Rect(pos[0]-pos[0]%50, pos[1]-pos[1]%50, 50, 50), 0)
             myboard[pos[0]//50][pos[1]//50].white = True
+            myboard[pos[0]//50][(pos[1]//50)+1].number+=1
+            myboard[pos[0]//50][(pos[1]//50)-1].number+=1
+            myboard[(pos[0]//50)+1][pos[1]//50].number+=1
+            myboard[(pos[0]//50)-1][pos[1]//50].number+=1
+            myboard[(pos[0]//50)+1][(pos[1]//50)-1].number+=1
+            myboard[(pos[0]//50)-1][(pos[1]//50)+1].number+=1
+            myboard[(pos[0]//50)+1][(pos[1]//50)+1].number+=1
+            myboard[(pos[0]//50)-1][(pos[1]//50)-1].number+=1
 
         if event.type == pygame.QUIT:
             pygame.quit()
